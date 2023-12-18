862292b0c6036183a45c5ae9180efc4b23ea15ae
Michael Gutierrez <michaelsg33@gmail.com>
2023-12-18 06:47:50 -0600

    reorganized into blueprints for login and blog

--- website/__init__.py
+++ website/__init__.py
@@ -4,31 +4,39 @@ from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
 # initialize the app with the extension
 db = SQLAlchemy()
-db.init_app(app)
 
 # Flat pages for blog posts
 pages = FlatPages()
 
-pages.init_app(app)
 
 # User session management setup
 login_manager = LoginManager()
-login_manager.init_app(app)
 login_manager.login_view= 'auth.login'
 
 
-from website.auth import bp as auth_bp
-app.register_blueprint(auth_bp, url_prefix='/auth')
-
-from website.blog import bp as blog_bp
-app.register_blueprint(blog_bp, url_prefix='/blog')
 
 
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from website import routes, jinja_ext
+
+    db.init_app(app)
+    pages.init_app(app)
+
+
+    login_manager.init_app(app)
+
+    from website.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from website.blog import bp as blog_bp
+    app.register_blueprint(blog_bp, url_prefix='/blog')
+
+    return app
+
+    from website import routes, jinja_ext, models
--- website/models.py
+++ website/models.py
@@ -8,48 +8,48 @@ from enum import Enum
 
 
 class AUTH(Enum):
-	ME = 1
-	SELECT = 2
-	FAMILY = 3
-	FRIENDS = 4
-	PUBLIC = 5
+    ME = 1
+    SELECT = 2
+    FAMILY = 3
+    FRIENDS = 4
+    PUBLIC = 5
 
 
 
 class User(UserMixin, db.Model):
-	id: so.Mapped[str] = so.mapped_column(sa.String(64),primary_key=True, unique=True)
-	username: so.Mapped[str] = so.mapped_column(
-		sa.String(64), index=True)
-	email: so.Mapped[str] = so.mapped_column(
-		sa.String(64), index=True)
-	picture: so.Mapped[Optional[str]] = so.mapped_column(
-		sa.String(64))
-	comments: so.WriteOnlyMapped['Comment'] = so.relationship(
-		back_populates='author')
-	log_ins: so.WriteOnlyMapped['LogIns'] = so.relationship(
-		back_populates='user')
-	auth: so.Mapped[AUTH] = so.mapped_column(
-		index=True, default=AUTH.PUBLIC)
+    id: so.Mapped[str] = so.mapped_column(sa.String(64),primary_key=True, unique=True)
+    username: so.Mapped[str] = so.mapped_column(
+        sa.String(64), index=True)
+    email: so.Mapped[str] = so.mapped_column(
+        sa.String(64), index=True)
+    picture: so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(64))
+    comments: so.WriteOnlyMapped['Comment'] = so.relationship(
+        back_populates='author')
+    log_ins: so.WriteOnlyMapped['LogIns'] = so.relationship(
+        back_populates='user')
+    auth: so.Mapped[AUTH] = so.mapped_column(
+        index=True, default=AUTH.PUBLIC)
 
-	@staticmethod
-	def get(id):
-		return db.session.get(User, id)
+    @staticmethod
+    def get(id):
+        return db.session.get(User, id)
 
 class LogIns(db.Model):
-	id: so.Mapped[int] = so.mapped_column(primary_key=True)
-	user_id: so.Mapped[str] = so.mapped_column(
-		sa.ForeignKey(User.id), index=True)
-	timestamp: so.Mapped[datetime] = so.mapped_column(
-		index=True, default=lambda: datetime.now(timezone.utc))
-	user: so.Mapped[User] = so.relationship(back_populates="log_ins")
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[str] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user: so.Mapped[User] = so.relationship(back_populates="log_ins")
 
 class Comment(db.Model):
-	id: so.Mapped[int] = so.mapped_column(primary_key=True)
-	body: so.Mapped[str] = so.mapped_column(sa.String(140))
-	timestamp: so.Mapped[datetime] = so.mapped_column(
-		index=True, default=lambda: datetime.now(timezone.utc))
-	user_id: so.Mapped[int] = so.mapped_column(
-		sa.ForeignKey(User.id), index=True)
-	author: so.Mapped[User] = so.relationship(back_populates="comments")
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates="comments")
 
 
