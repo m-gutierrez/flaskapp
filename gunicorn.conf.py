ea09ae4b9bf5a1352638488cb25f7689d7c50819
Michael Gutierrez <michaelsg33@gmail.com>
2023-12-28 06:11:54 -0600

    added gunicorn config file

---
+++ gunicorn.conf.py
@@ -0,0 +1,18 @@
+import multiprocessing
+import os
+
+
+
+wsgi_app = run:app
+bind = "localhost:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+
+
+keyfile = os.environ.get('WEB_KEY')
+certfile = os.environ.get('WEB_CERT')
+
+
+accesslog = '/var/log/flaskapp/access.log'
+errorlog = '/var/log/flaskapp/error.log'
+
+disable_redirect_access_to_syslog = True
